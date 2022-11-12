fde50ecfbafe296569eb9473dcd4499bdf7d2ff5
Costa <booa@gft.com>
2022-11-12 18:32:46 -0300

    login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './views/login/login.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// PRIMENG MODULES
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    // PRIMENG MODULES
+    CardModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/views/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { faCoffee } from '@fortawesome/angular-fontawesome';
+// import { faCoffee } from '@fortawesome/fontawesome-common-types';
+// import { faCoffee } from '@fortawesome/fontawesome-svg-core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
